78576e8a5d2ecd9e30f4686336553ad2a894c89e
Iyana Medlock <iyanamedlock@gmail.com>
2020-12-10 00:59:46 -0600

    added more chars to array, improved code a lil

--- script.js
+++ script.js
@@ -36,19 +36,21 @@ window.onload = function(){
               "h","i","j","k","l","m","n","o","p","q","r",
               "s","t","u","v","w","x","y","z","1","2","3",
               "4","5","6","7","8","9","0","!","@","#","$",
-              "%","&"]
+              "%","&","?","+",";",":","~"]
 
   btn.onclick = function(){
+    var textArea = document.getElementById('password');
+    textArea.value = "";
+
     var userPref = prompt("How long do you want you password? (Must be at least 8 - 128 characters)");
     userPref = parseInt(userPref)
     if (userPref >= 8 && userPref <= 128){
       var x = "";
       for (var i = 0; i < userPref; i++){
         var password = keys[Math.floor(Math.random() * keys.length)]
-        x +=password+"";
+        x +=password;
       } 
       
-      var textArea = document.getElementById('password');
       textArea.value = x;
       textArea.focus();
       textArea.select();
